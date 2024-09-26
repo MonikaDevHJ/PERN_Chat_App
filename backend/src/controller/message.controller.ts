6601649bf5e330612b2043bbfad7b7e52232f825
Monika H J <monikahj7@gmail.com>
2024-09-26 12:50:59 +0530

    BAck end part is littel done

--- backend/src/controller/message.controller.ts
+++ backend/src/controller/message.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { json, Request, Response } from "express";
 import prisma from "../db/prisma";
 
 export const sendMessage = async (req:Request, res:Response)=>{
@@ -60,3 +60,59 @@ export const sendMessage = async (req:Request, res:Response)=>{
     };
     
 };
+
+
+export const getMessages = async (req:Request, res:Response)=>{
+    try{
+        const {id:userToChatId} = req.params;
+        const senderId = req.user.id;
+
+        const conversation = await prisma.conversation.findFirst({
+            where :{
+                participantIds : {
+                    hasEvery : [senderId,userToChatId]
+                }
+            },
+            include : {
+                message : {
+                    orderBy : {
+                        createdAt : "asc"
+                    }
+                }
+            }
+        })
+
+        if(!conversation){
+            return res.status(200).json([]);
+        }
+
+    }catch(error:any){
+        console.log("Error in getMessage:", error.message);
+        res.status(500).json({error:"Internal server error"});
+    }
+}
+
+
+export const getUserForSidebar = async (req:Request, res:Response)=>{
+    try{
+        const authUserId = req.user.id;
+
+        const users = await prisma.user.findMany({
+            where :{
+                id:{
+                    not : authUserId,
+                },
+            },
+            select : {
+                id : true,
+                fullName : true,
+                profilePic : true,
+            }
+        });
+        res.status(200).json(users);
+    }catch (error : any){
+        console.log("Error in getUsersForSidebars:", error.message);
+        res.status(500).json({error : "Internal server error"});
+    }
+
+}
--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import proctecRoute from "../MiddleWar/ProtectRoute";
-import { sendMessage } from "../controller/message.controller";
+import {
+  getMessages,
+  getUserForSidebar,
+  sendMessage
+} from "../controller/message.controller";
 
 const router = express.Router();
 
+router.get("/conversations", proctecRoute, getUserForSidebar);
+router.get("/:id", proctecRoute, getMessages);
 
 router.post("/send/:idx", proctecRoute, sendMessage);
 
 export default router; // Don't forget to export the router!
 
-  
+
+// Todo : Add socket.io to the server
+// Todo : Configure this server for  the deployement
